66b34c2566bece7e4252b2c3026fb98efcb87026
wonnerky <gandet09@gmail.com>
2019-11-01 17:20:21 +0900

    fourth version

--- run.py
+++ run.py
@@ -11,15 +11,18 @@ __all__ = ['driver']
 path = "./chromedriver"
 driver = webdriver.Chrome(path)
 driver.get("https://www.plabfootball.com/")
+avaMatch = False
+count = 0
 
 # match number로 node 찾고 신청가능 해지면 알림이 온다.
 while 1:
     driver.refresh()
 
     # match number 찾아서 적어 넣기
-    element0 = driver.find_elements(By.XPATH, "//a[@href='/match/4936/'] | //a[@href='/match/4938/']")
+    element0 = driver.find_elements(By.XPATH, "//a[@href='/match/6093/'] | //a[@href='/match/6083/'] | //a[@href='/match/6007/']")
     if not element0:
-        os.system('say "원하시는 게임이 없어요"')
+        os.system('say "매치가 없어요"')
+        print("매치가 없어요")
         break
     element = []
     i = 0
@@ -33,21 +36,18 @@ while 1:
         ele_len = ele_len - 1
 
     for ele in element:
-        print(ele.text)
+        if ele.text != u"마감":
+            avaMatch = True
+            break
 
     # class 로 만들어서 하면 더 좋을 듯
-    if element[0].text != u"마감":
+    if avaMatch == True:
         os.system('say "자리가 났어요"')
-        break
-    elif element[1].text != u"마감":
-        os.system('say "자리가 났어요"')
+        print('자리가 났어요')
         break
     else:
+        count = count + 1
+        print('{}번째 체크 중...'.format(count))
         time.sleep(60)
-
-
-
-
-
 
 driver.quit()
