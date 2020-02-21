5984fd0d263d885d23f8297c8161b82f293d8043
wonnerky <gandet09@gmail.com>
2020-02-21 14:43:45 +0900

    delay add

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import time
 from selenium import webdriver
-from selenium.webdriver.common.by import  By
+from selenium.webdriver.common.by import By
 from slacker import Slacker
 import parameter
 import os
@@ -31,28 +31,31 @@ token = parameter.token
 slack = Slacker(token)
 # matches 객체에 원하는 매치 시간과 장소 상태 입
 lookingMatches = []
+lookingMatches.append(Matches('19:00', '용산 아이파크몰 (1구장/레알)'))
 lookingMatches.append(Matches('21:00', '용산 아이파크몰 (1구장/레알)'))
-lookingMatches.append(Matches('23:00', '용산 아이파크몰 (1구장/레알)'))
 
 
 while 1:
     driver.refresh()
+    time.sleep(5)
     nowMatches = []
     elements = driver.find_elements(By.XPATH, '//div[@class="list--match-schedule--container"]/ul/li')
 
     # elements[i].find_element_by_class_name("list--match-schedule__time") Time 정보 Node
     # elements[i].find_element(By.XPATH, '//a/div[@class="list--match-schedule__info"]/h3') Ground 정보 Node
     # elements[i].find_element(By.XPATH, '//a/div[@class="list--match-schedule__status"]/div/p') Ground 정보 Node
-
+    print(elements)
     for i in range(len(elements)):
         time_element = elements[i].find_element_by_class_name("list--match-schedule__time")
         ground_element = elements[i].find_element_by_class_name("list--match-schedule__info")
         ground_element = ground_element.find_element_by_xpath('h3')
         status_element = elements[i].find_element_by_class_name("list--match-schedule__status")
+        # print(time_element.text,ground_element.text,status_element.text)
         for j in range(len(lookingMatches)):
             if time_element.text == lookingMatches[j].time and ground_element.text == lookingMatches[j].ground:
                 nowMatches.append(Matches(lookingMatches[j].time,lookingMatches[j].ground,status_element.text))
-
+    # for i in range(len(nowMatches)):
+    #     print(nowMatches[i].time,nowMatches[i].ground,nowMatches[i].status)
 
     if not nowMatches:
         os.system('say "매치가 없어요"')
@@ -60,7 +63,7 @@ while 1:
         break
 
     for i in range(len(nowMatches)):
-        if nowMatches[i].status != u"마감":
+        if nowMatches[i].status != "마감":
             avaMatch = True
             break
 
@@ -73,6 +76,6 @@ while 1:
     else:
         count = count + 1
         print('{}번째 체크 중...'.format(count))
-        time.sleep(60)
+        time.sleep(30)
 
 driver.quit()
